f145dc96cc7be4ef2af5469e37247146f2b7b9df
Rohan Kothapalli <rohanricky0609@gmail.com>
2018-10-24 16:37:10 +0530

    changes

---
+++ messaging_queue.py
@@ -0,0 +1,10 @@
+from multiprocessing import Queue
+
+class messaging_queue():
+	def __init__():
+		self.queue=Queue()
+
+	def send(text):
+		self.queue.put(text)
+
+	def 
---
+++ s2t.py
@@ -0,0 +1,70 @@
+# coding: utf-8
+import speech_recognition as sr
+import pyttsx3 as pyttsx
+
+headers = {'''user-agent':'Chrome/53.0.2785.143'''}
+#speak=wicl.Dispatch("SAPI.SpVoice")
+
+speak = pyttsx.init()
+
+#async def utlisCheck(check):
+
+def cont(text=None):
+	if text:
+		events(text[1],text[2:])
+	else:
+		r=sr.Recognizer()
+		with sr.Microphone() as source:
+			r.adjust_for_ambient_noise(source)
+			audio=r.listen(source)
+
+		try:
+			x = r.recognize_google(audio)
+			print(x)
+			x=x.lower()
+			link=x.split()
+			if check.Fuck_internetCheck is 0:
+				threading.Thread(target=req(x)).start()
+			else:
+				threading.Thread(target=events(x,link)).start()
+
+		except:
+			notify("Sorry Boss, Try again!")
+			cont()
+
+if __name__=="__main__":
+#	check = ctypes.CDLL(os.path.dirname(os.path.realpath(__file__))+'/lib/cpp/libint.so')
+#	asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+#	loop=asyncio.get_event_loop()
+#	for signame in ('SIGINT','SIGTERM'):
+#		loop.add_signal_handler(getattr(signal,signame),lambda: asyncio.ensure_future(ask_exit(signame)))
+	while True:
+		r=sr.Recognizer()
+		with sr.Microphone() as source:
+			r.adjust_for_ambient_noise(source,duration=1)
+			print("Say Something")
+			audio=r.listen(source,timeout=10, phrase_time_limit=5)
+
+		try:
+			x = r.recognize_google(audio)
+			x=x.lower()
+			threading.Thread(target=req(x)).start()
+		except:
+			print("No audio detected")
+'''
+	try:
+		print("Running")
+		loop.run_forever()
+	finally:
+		loop.close()
+
+if x=="jarvis" and first is 0:
+	speak.say("Yes Boss")
+	speak.runAndWait()
+	x=x.split()
+	text=x[1:]
+	first=first+1
+	cont()
+else:
+	threading.Thread(target=req(x)).start()
+'''
